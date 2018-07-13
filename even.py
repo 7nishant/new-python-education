c45dc3135ae27427ea9c1651ec3128df4377deaa
7nishant <nishant4735@gmail.com>
2018-07-12 17:50:31 -0700

    commiting even one

---
+++ even.py
@@ -0,0 +1,10 @@
+def result(n):
+    for num in range(0, 10000):
+        if (num % 2 == 0):
+            print(num)
+        else:
+            pass
+if __name__ == "__main__":
+    n = 0
+print(result(n))
+
